87d626cd2a5097b65d17073b5dc4a21121aeb7a5
Proximus <user@proximustech.net>
2024-03-17 03:05:02 -0500

    Ok list inline editing

--- src/plugins/ajax_views/routes/ajax_views_routes.ts
+++ src/plugins/ajax_views/routes/ajax_views_routes.ts
@@ -1,12 +1,22 @@
 import Router from "koa-router"
+import koaBody from "koa-body"
+
+let data = {
+    "0":"1st Record",
+    "1":"2nd Record",
+}
 
 let viewVars ={
-
+    data: {},
+    id : '',
+    title : ''
 }
 const router = new Router();
 
+
 router.get('/ajax_views/content', async (ctx) => {
     try {
+        viewVars.data = data
         return ctx.render('plugins/ajax_views/views/content', viewVars);
     } catch (error) {
         console.error(error)
@@ -14,7 +24,22 @@ router.get('/ajax_views/content', async (ctx) => {
 })
 router.get('/ajax_views/detail', async (ctx) => {
     try {
+        //@ts-ignore
+        viewVars.id = ctx.request.query.id
+        //@ts-ignore
+        viewVars.title = data[ctx.request.query.id]
         return ctx.render('plugins/ajax_views/views/detail', viewVars);
+    } catch (error) {
+        console.error(error)
+    }
+})
+router.post('/ajax_views/save', koaBody() ,async (ctx) => {
+    try {
+        //@ts-ignore
+        data[ctx.request.body["id"]] = ctx.request.body["title"]
+        viewVars.id = ctx.request.body["id"]
+        viewVars.title = ctx.request.body["title"]
+        return ctx.render('plugins/ajax_views/views/save', viewVars);
     } catch (error) {
         console.error(error)
     }
