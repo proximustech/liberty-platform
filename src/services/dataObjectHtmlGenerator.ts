31922b229c11e5492d2b1644e1964c1c7781e52f
Proximus <user@proximustech.net>
2024-09-05 01:27:56 -0500

    Html Generator. Enable text area element

--- src/services/dataObjectHtmlGenerator.ts
+++ src/services/dataObjectHtmlGenerator.ts
@@ -16,6 +16,27 @@ export const HtmlDataObjectFieldRender: any = (dataObjectName:string,fieldName:s
         <input id='${dataObjectName}_${fieldName}' class='form-control' type='text' value='${fieldValue}' oninput="${dataObjectName}_${fieldName}_listener(this)" />
         <label id='${dataObjectName}_${fieldName}_validation_message' style='font-size:15px;color:red;margin-left:12px'></label>
         <br>
+
+        <script>
+            function ${dataObjectName}_${fieldName}_listener(element){
+                app.module_data.${dataObjectName}_form.${dataObjectName}.${fieldName}=element.value
+
+                regexpValidator = new RegExp("${validationRegexp}")
+                if(!regexpValidator.test(element.value)){
+                    document.getElementById('${dataObjectName}_${fieldName}_validation_message').innerHTML='${validationMessage}'
+                }
+                else {
+                    document.getElementById('${dataObjectName}_${fieldName}_validation_message').innerHTML=''
+                }
+            }
+        </script>
+        `       
+    }
+    else if (inputType=="text_area") {
+        html+=`
+        <label style='margin-bottom:7px;font-size:17px'>${label}</label>
+        <textarea id='${dataObjectName}_${fieldName}' class='form-control' oninput="${dataObjectName}_${fieldName}_listener(this)">${fieldValue}</textarea>
+        <label id='${dataObjectName}_${fieldName}_validation_message' style='font-size:15px;color:red;margin-left:12px'></label>
         <br>
 
         <script>
