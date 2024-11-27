bf4cbab902c90d12c2dababc129b8f85845e0b7b
Julián Arias <mail@proximustech.net>
2024-11-26 21:13:30 -0500

    Users. Bootstraping script

--- src/plugins/users_control/scripts/bootstrap.ts
+++ src/plugins/users_control/scripts/bootstrap.ts
@@ -1,22 +1,44 @@
-import { UserService } from "../services/UserService";
-import { RoleService } from "../services/RoleService";
+import {AuthorizerCasbinMongo as AuthorizerCasbin} from "../../../services/authorizer_casbin_mongo";
+
 import { UserDataObject } from "../dataObjects/UserDataObject";
 import { RoleDataObject } from "../dataObjects/RoleDataObject";
 
-let userService = new UserService()
-let roleService = new RoleService()
+import { UserService } from "../services/UserService";
+import { RoleService } from "../services/RoleService";
 
-let role = new RoleDataObject()
-role.name = "Administrator"
+async function main() {
 
-let user = new UserDataObject()
-user.role_uuid = role.uuid
-user.email="admin@admin.com"
-user.password = "admin"
+    let userService = new UserService()
+    let roleService = new RoleService()
 
-let main = async() => {
-    await roleService.create(role)
+    let role = new RoleDataObject()
+    role.name = "Administrator"
+
+    let roleUuid=await roleService.create(role)
+    
+    let user = new UserDataObject()
+    user.role_uuid = roleUuid
+    user.email="admin@admin.com"
+    user.password = "admin"
+
     await userService.create(user)
+
+    const permissions: any = [
+        {resource:"users_control.user",permission:"read"},
+        {resource:"users_control.user",permission:"write"},
+        {resource:"users_control.role",permission:"read"},
+        {resource:"users_control.role",permission:"write"},
+    ]
+    
+    const authorizer: AuthorizerCasbin = new AuthorizerCasbin()
+    await authorizer.initialize()
+    for (let index = 0; index < permissions.length; index++) {
+        const permission = permissions[index];
+        let result:Boolean = await authorizer.enforcer.addPolicy(roleUuid,permission.resource, permission.permission)
+    }   
+
     process.exit()
+    
 }
+
 main()
