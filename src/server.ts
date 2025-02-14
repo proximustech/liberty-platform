996228f83b0f2235f92f2bfe1f0a3eb10c4faed5
Julián Arias <mail@proximustech.net>
2025-02-14 11:27:29 -0500

    Passport. Prepare openidconnect

--- src/server.ts
+++ src/server.ts
@@ -37,9 +37,11 @@ export let eventEmitter : EventEmitter
   //app.use(koaBody())
 
   app.keys = ['lkaweob923jkpselld34k'];
+  //app.use(session({cookie: { secure: false }},app))
   app.use(session(app))
   
   require('./auth/local_auth')
+  //require('./auth/oidc_auth')
   app.use(passport.initialize());
   app.use(passport.session());
 
